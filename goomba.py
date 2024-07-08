e7888b92c6cb023153f6757eb3fdf6ec3c018fa6
Mark Raspopov <63547351+coderMassive@users.noreply.github.com>
2024-07-07 21:56:18 -0700

    make goomba slower

--- goomba.py
+++ goomba.py
@@ -2,5 +2,5 @@ class goomba(gameObject):
     def __init__(self, x, y):
         super().__init__(x, y, 1, 1)
         self.color = (255, 0, 0)
-        self.speed = 0.125 # used in the general update function
+        self.speed = 0.02 # used in the general update function
 
